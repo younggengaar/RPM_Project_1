2dc492ff0f86406db22524548c0ef5f48a1df9fe
liza <123323177+younggengaar@users.noreply.github.com>
2024-03-13 16:11:53 +0200

    Add files via upload

---
+++ РПМ Практическая работа 1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace РПМ_Практическая_работа_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            c:
+            Console.Write("Введите номер задания:");
+            int n= int.Parse(Console.ReadLine());
+            switch(n)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Ввычислить среднее значение элементов массива из n чисел.");
+                        Console.Write("Введите размер массива:");
+                        int kol = int.Parse(Console.ReadLine());
+                        int[]numb = new int[kol];
+                        int sum = 0;
+                        int count = 0;
+                        for (int i = 0;i<kol;i++)
+                        {
+                            Console.Write($"Введите {i + 1} элемент массива:");
+                            numb[i] = int.Parse(Console.ReadLine());
+                            count++;
+                        }
+                        foreach(int num in numb)
+                        {
+                            sum += num;
+                        }
+                        double sr_arf = sum / count;
+                        Console.WriteLine($"Среднее значение {sr_arf}");
+                        Console.ReadKey();
+                    }
+                    goto c;
+            }
+        }
+    }
+}
